9b40811fbbb8b80173f7cb06a85ef1b2dc47147f
Marcelo Soares <marcelohsoares142@gmail.com>
2024-05-19 12:38:14 -0300

    Adicionado a interação com o usúario com JS

---
+++ main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("form");
+const contatos = [];
+const telefones = [];
+
+let linhas = "";
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // Remove o comportamento de atualizar a página ao clicar em submit
+
+  const addContact = document.getElementById("contact");
+  const addNum = document.getElementById("num");
+
+  if (contatos.includes(addContact.value) && telefones.includes(addNum.value)) {
+    alert("Você já adicinou esse contato!");
+  } else if (telefones.includes(addNum.value)) {
+    alert("Você tem um contato existente com esse número!");
+  } else if (addNum.value.length !== 11) {
+    alert("O telefone deve conter exatamente 11 dígitos");
+  } else {
+    contatos.push(addContact.value);
+    telefones.push(addNum.value);
+
+    let linha = "<tr>";
+    linha += `<td>${addContact.value}</td>`;
+    linha += `<td>${addNum.value}</td>`;
+    linha += "</tr>";
+    linhas += linha;
+
+    // Limpa os campos de input após adicionar
+    addContact.value = "";
+    addNum.value = "";
+
+    atualizaTabela();
+  }
+});
+
+function atualizaTabela() {
+  const corpoTabela = document.querySelector(".tbody");
+  corpoTabela.innerHTML = linhas; // Atualiza a tabela com as novas linhas
+}
